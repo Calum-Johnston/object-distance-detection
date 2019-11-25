8a470c6099518bb44de869ad0d0bfb9c0370b9b1
Calum-Johnston <calumphilipjohnston@gmail.com>
2019-11-25 10:50:18 +0000

    Successfully combined YOLO and stereo disparity

--- distance-quantifier-master/combination.py
+++ distance-quantifier-master/combination.py
@@ -30,59 +30,6 @@ import numpy as np
 import os
 
 ################################################################################
-# === Initialisation of the required variables === #
-################################################################################
-
-################################################################################
-
-################################################################################
-# where is the data?
-master_path_to_dataset = os.path.dirname(os.path.realpath(__file__)) + "\TTBB-durham-02-10-17-sub10";
-master_path_to_yolo_resources = os.path.dirname(os.path.realpath(__file__)) + "\object-detection-master\yolo resources"; 
-directory_to_cycle_left = "left-images";     
-directory_to_cycle_right = "right-images";  
-
-################################################################################
-# YOLO INITIALISATION 
-################################################################################
-# init YOLO CNN object detection model 
-confThreshold = 0.5  # Confidence threshold
-nmsThreshold = 0.4   # Non-maximum suppression threshold
-inpWidth = 416       # Width of network's input image
-inpHeight = 416      # Height of network's input image
-
-# Load names of classes from file
-classesFile = os.path.join(master_path_to_yolo_resources, "coco.names");
-classes = None
-with open(classesFile, 'rt') as f:
-    classes = f.read().rstrip('\n').split('\n')
-
-# load configuration and weight files for the model and load the network using them
-modelConfiguration = os.path.join(master_path_to_yolo_resources, "yolov3.cfg");
-modelWeights = os.path.join(master_path_to_yolo_resources, "yolov3.weights");
-
-net = cv2.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
-output_layer_names = getOutputsNames(net)
-
- # defaults DNN_BACKEND_INFERENCE_ENGINE if Intel Inference Engine lib available or DNN_BACKEND_OPENCV otherwise
-net.setPreferableBackend(cv2.dnn.DNN_BACKEND_DEFAULT)
-
-# change to cv2.dnn.DNN_TARGET_CPU (slower) if this causes issues (should fail gracefully if OpenCL not available)
-net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
-
-
-################################################################################
-# DISPARITY INITIALISATION 
-################################################################################
-# setup the disparity stereo processor to find a maximum of 128 disparity values
-# (adjust parameters if needed - this will effect speed to processing)
-max_disparity = 128
-stereoProcessor = cv2.StereoSGBM_create(0, max_disparity, 21);
-
-
-
-
-################################################################################
 # === YOLO Object Detection Functions === #
 ################################################################################
 
@@ -178,11 +125,22 @@ def getOutputsNames(net):
     return [layersNames[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 
 
-################################################################################
-# === B
-################################################################################
 
 
+
+
+################################################################################
+# === Initialisation of the required variables === #
+################################################################################
+
+################################################################################
+# Initialisation of dataset
+
+# where is the data?
+master_path_to_dataset = os.path.dirname(os.path.realpath(__file__)) + "\TTBB-durham-02-10-17-sub10";
+master_path_to_yolo_resources = os.path.dirname(os.path.realpath(__file__)) + "\yolo resources"; 
+directory_to_cycle_left = "left-images";     
+directory_to_cycle_right = "right-images"; 
 
 # resolve full directory location of data set for left / right images
 full_path_directory_left =  os.path.join(master_path_to_dataset, directory_to_cycle_left);
@@ -194,47 +152,94 @@ left_file_list = sorted(os.listdir(full_path_directory_left));
 
 
 ################################################################################
+# Initialisation of YOLO Object Detection
 
-# define display window name + trackbar
+# init YOLO CNN object detection model 
+confThreshold = 0.5  # Confidence threshold
+nmsThreshold = 0.4   # Non-maximum suppression threshold
+inpWidth = 416       # Width of network's input image
+inpHeight = 416      # Height of network's input image
 
-windowName = 'YOLOv3 object detection: ' + modelWeights
+# Load names of classes from file
+classesFile = os.path.join(master_path_to_yolo_resources, "coco.names");
+classes = None
+with open(classesFile, 'rt') as f:
+    classes = f.read().rstrip('\n').split('\n')
+
+# load configuration and weight files for the model and load the network using them
+modelConfiguration = os.path.join(master_path_to_yolo_resources, "yolov3.cfg");
+modelWeights = os.path.join(master_path_to_yolo_resources, "yolov3.weights");
+
+net = cv2.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
+output_layer_names = getOutputsNames(net)
+
+ # defaults DNN_BACKEND_INFERENCE_ENGINE if Intel Inference Engine lib available or DNN_BACKEND_OPENCV otherwise
+net.setPreferableBackend(cv2.dnn.DNN_BACKEND_DEFAULT)
+
+# change to cv2.dnn.DNN_TARGET_CPU (slower) if this causes issues (should fail gracefully if OpenCL not available)
+net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
+
+
+
+################################################################################
+# Initialisation of Disparity 
+
+# setup the disparity stereo processor to find a maximum of 128 disparity values
+# (adjust parameters if needed - this will effect speed to processing)
+max_disparity = 128
+stereoProcessor = cv2.StereoSGBM_create(0, max_disparity, 21);
+
+
+################################################################################
+# Define display window name + trackbar
+
+windowName = 'Object distance detection: ' + modelWeights
 cv2.namedWindow(windowName, cv2.WINDOW_NORMAL)
 trackbarName = 'reporting confidence > (x 0.01)'
 cv2.createTrackbar(trackbarName, windowName , 0, 100, on_trackbar)
 
-################################################################################
 
-# if command line arguments are provided try to read video_name
-# otherwise default to capture from attached camera
+
+
+
+
+################################################################################
+# === Main Program - Calculations === #
+################################################################################
 
 for filename_left in left_file_list:
 
     # from the left image filename get the correspondoning right image
-
     filename_right = filename_left.replace("_L", "_R");
     full_path_filename_left = os.path.join(full_path_directory_left, filename_left);
     full_path_filename_right = os.path.join(full_path_directory_right, filename_right);
 
     # for sanity print out these filenames
-
     print(full_path_filename_left);
     print(full_path_filename_right);
     print();
 
-    # check the file is a PNG file (left) and check a correspondoning right image
-    # actually exists
+    # check the file is a PNG file (left) and check a correspondoning right image actually exists
     while(cv2.waitKey(1) < 0):
         if ('.png' in filename_left) and (os.path.isfile(full_path_filename_right)) :
 
+            ################################################################################
+            # Setup of windows, images, timers, etc
+            ################################################################################
             # start a timer (to see how long processing and display takes)
             start_t = cv2.getTickCount()
 
             # create window by name (as resizable)
             cv2.namedWindow(windowName, cv2.WINDOW_NORMAL)
 
-            # read left image
+            # read left and right images
             imgL = cv2.imread(full_path_filename_left, cv2.IMREAD_COLOR)
+            imgR = cv2.imread(full_path_filename_right, cv2.IMREAD_COLOR)
 
+
+            ################################################################################
+            # YOLO Object Detection Calculations + Function Calls
+            ################################################################################
             # create a 4D tensor (OpenCV 'blob') from image frame (pixels scaled 0->1, image resized)
             blob = cv2.dnn.blobFromImage(imgL, 1/255, (inpWidth, inpHeight), [0,0,0], 1, crop=False)
 
@@ -248,6 +253,50 @@ for filename_left in left_file_list:
             confThreshold = cv2.getTrackbarPos(trackbarName,windowName) / 100
             classIDs, confidences, boxes = postprocess(imgL, results, confThreshold, nmsThreshold)
 
+
+            ################################################################################
+            # Stereo Disparity Calculations + Function Calls
+            ################################################################################
+            # remember to convert to grayscale (as the disparity matching works on grayscale)
+            # N.B. need to do for both as both are 3-channel images
+            grayL = cv2.cvtColor(imgL,cv2.COLOR_BGR2GRAY);
+            grayR = cv2.cvtColor(imgR,cv2.COLOR_BGR2GRAY);
+
+            # perform preprocessing - raise to the power, as this subjectively appears
+            # to improve subsequent disparity calculation
+            grayL = np.power(grayL, 0.75).astype('uint8');
+            grayR = np.power(grayR, 0.75).astype('uint8');
+
+            # compute disparity image from undistorted and rectified stereo images
+            # that we have loaded
+            # (which for reasons best known to the OpenCV developers is returned scaled by 16)
+            disparity = stereoProcessor.compute(grayL,grayR);
+
+            # filter out noise and speckles (adjust parameters as needed)
+            dispNoiseFilter = 5; # increase for more agressive filtering
+            cv2.filterSpeckles(disparity, 0, 4000, max_disparity - dispNoiseFilter);
+
+            # scale the disparity to 8-bit for viewing
+            # divide by 16 and convert to 8-bit image (then range of values should
+            # be 0 -> max_disparity) but in fact is (-1 -> max_disparity - 1)
+            # so we fix this also using a initial threshold between 0 and max_disparity
+            # as disparity=-1 means no disparity available
+
+            _, disparity = cv2.threshold(disparity,0, max_disparity * 16, cv2.THRESH_TOZERO);
+            disparity_scaled = (disparity / 16.).astype(np.uint8);
+
+            # crop disparity to chop out left part where there are with no disparity
+            # as this area is not seen by both cameras and also
+            # chop out the bottom area (where we see the front of car bonnet)
+            crop_disparity = False
+            if (crop_disparity):
+                width = np.size(disparity_scaled, 1);
+                disparity_scaled = disparity_scaled[0:390,135:width];
+
+
+            ################################################################################
+            # Resulting Distance Calculations + Drawing
+            ################################################################################
             # draw resulting detections on image
             for detected_object in range(0, len(boxes)):
                 box = boxes[detected_object]
