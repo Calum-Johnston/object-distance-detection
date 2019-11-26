dea9210142fda30b4cd9527b47580cea7002d214
Calum-Johnston <calumphilipjohnston@gmail.com>
2019-11-26 20:15:11 +0000

    Added surf detection

--- distance-quantifier-master/combination.py
+++ distance-quantifier-master/combination.py
@@ -22,6 +22,7 @@
 
 ################################################################################
 
+import time
 import cv2
 import argparse
 import sys
@@ -236,10 +237,7 @@ camera_focal_length_m = 4.8 / 1000          # focal length in metres (4.8 mm)
 stereo_camera_baseline_m = 0.2090607502     # camera baseline in metres
 
 image_centre_h = 262.0;
-image_centre_w = 474.5;
-
-crop_disparity = False; # display full or cropped disparity image
-pause_playback = False; # pause until key press after each image
+image_cent4re_w = 474.5;
 
 
 
@@ -322,21 +320,20 @@ for filename_left in left_file_list:
         # compute disparity image from undistorted and rectified stereo images
         # that we have loaded
         # (which for reasons best known to the OpenCV developers is returned scaled by 16)
-        disparity = stereoProcessor.compute(grayL,grayR);
+        disparity = stereoProcessor.compute(grayL, grayR);
 
         # filter out noise and speckles (adjust parameters as needed)
         dispNoiseFilter = 5; # increase for more agressive filtering
         cv2.filterSpeckles(disparity, 0, 4000, max_disparity - dispNoiseFilter);
 
-        #Another type of noise filtering:
-        # https://rdmilligan.wordpress.com/2016/05/23/disparity-of-stereo-images-with-python-and-opencv/
+        # Sparse stereo and issues
+        # https://www.intechopen.com/online-first/efficient-depth-estimation-using-sparse-stereo-vision-with-other-perception-techniques
 
         # scale the disparity to 8-bit for viewing
         # divide by 16 and convert to 8-bit image (then range of values should
         # be 0 -> max_disparity) but in fact is (-1 -> max_disparity - 1)
         # so we fix this also using a initial threshold between 0 and max_disparity
         # as disparity=-1 means no disparity available
-
         _, disparity = cv2.threshold(disparity,0, max_disparity * 16, cv2.THRESH_TOZERO);
         disparity_scaled = (disparity / 16.).astype(np.uint8);
 
