35cc5c834c7a386255517fb90bafbbd4df340670
Calum-Johnston <calumphilipjohnston@gmail.com>
2019-11-27 11:54:04 +0000

    Implemented Flann based matching using ORB feature detection

--- other/sparse_disparity.py
+++ other/sparse_disparity.py
@@ -18,10 +18,77 @@ import sys
 import math
 import numpy as np
 import os
+from matplotlib import pyplot as plt
 
 # where is the data?
 
 master_path_to_dataset = os.path.dirname(os.path.realpath(__file__)) + "\TTBB-durham-02-10-17-sub10";
 master_path_to_yolo_resources = os.path.dirname(os.path.realpath(__file__)) + "\yolo resources"; 
 directory_to_cycle_left = "left-images";     
-directory_to_cycle_right = "right-images";  
+directory_to_cycle_right = "right-images";
+
+# resolve full directory location of data set for left / right images
+full_path_directory_left =  os.path.join(master_path_to_dataset, directory_to_cycle_left);
+full_path_directory_right =  os.path.join(master_path_to_dataset, directory_to_cycle_right);
+
+# get a list of the left image files and sort them (by timestamp in filename)
+left_file_list = sorted(os.listdir(full_path_directory_left));
+
+for filename_left in left_file_list:
+
+    # from the left image filename get the correspondoning right image
+
+    filename_right = filename_left.replace("_L", "_R");
+    full_path_filename_left = os.path.join(full_path_directory_left, filename_left);
+    full_path_filename_right = os.path.join(full_path_directory_right, filename_right);
+
+    # for sanity print out these filenames
+
+    print(full_path_filename_left);
+    print(full_path_filename_right);
+    print();
+
+    feature_object = cv2.ORB_create(800)
+    FLANN_INDEX_LSH = 6
+    index_params= dict(algorithm = FLANN_INDEX_LSH,
+        table_number = 6, # 12
+        key_size = 12,     # 20
+        multi_probe_level = 1) #2
+    search_params = dict(checks=50)
+
+    matcher = cv2.FlannBasedMatcher(index_params,search_params)
+
+    if ('.png' in filename_left) and (os.path.isfile(full_path_filename_right)) :
+        
+        # start a timer (to see how long processing and display takes)
+        start_t = cv2.getTickCount()
+
+        imgL = cv2.imread(full_path_filename_left, cv2.IMREAD_COLOR)
+        imgR = cv2.imread(full_path_filename_right, cv2.IMREAD_COLOR)
+
+        kpL, desL = feature_object.detectAndCompute(imgL,None)
+        kpR, desR = feature_object.detectAndCompute(imgR,None)
+
+        matches = []
+        if(len(desR > 0)):
+            matches = matcher.knnMatch(desL, desR, k = 2)
+        
+        # Need to draw only good matches, so create a mask
+        good_matches = []
+
+        # perform a first match to second match ratio test as original SIFT paper (known as Lowe's ration)
+        # using the matching distances of the first and second matches
+        try:
+            for (m,n) in matches:
+                if m.distance < 0.7*n.distance:
+                    good_matches.append(m)
+        except ValueError:
+            print("caught error - no matches from current frame")
+
+        draw_params = dict(matchColor = (0,255,0), 
+                           singlePointColor = (255,0,0), 
+                           flags = 0)
+        #display_matches = cv2.drawMatches(imgL,kpL,imgR,kpR,good_matches,None,**draw_params)
+        cv2.imshow("hi",imgL)
+
+        cv2.waitKey()
