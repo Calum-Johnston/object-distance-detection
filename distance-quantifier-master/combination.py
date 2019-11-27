e06029b957e793a07c08019ffbb5692cca4d9315
Calum-Johnston <calumphilipjohnston@gmail.com>
2019-11-27 16:17:12 +0000

    Wrote another version of YOLO that simply returns data

--- distance-quantifier-master/combination.py
+++ distance-quantifier-master/combination.py
@@ -29,6 +29,7 @@ import sys
 import math
 import numpy as np
 import os
+import yolo_detection as yolo
 
 ################################################################################
 # === YOLO Object Detection Functions === #
@@ -103,69 +104,6 @@ def getBoxDistance(disparity_scaled, box):
         return averageDistance 
     return 0       
 
-#####################################################################
-# Remove the bounding boxes with low confidence using non-maxima suppression
-# image: image detection performed on
-# results: output from YOLO CNN network
-# threshold_confidence: threshold on keeping detection
-# threshold_nms: threshold used in non maximum suppression
-def postprocess(image, results, threshold_confidence, threshold_nms):
-    frameHeight = image.shape[0]
-    frameWidth = image.shape[1]
-
-    classIds = []
-    confidences = []
-    boxes = []
-
-    # Scan through all the bounding boxes output from the network and..
-    # 1. keep only the ones with high confidence scores.
-    # 2. assign the box class label as the class with the highest score.
-    # 3. construct a list of bounding boxes, class labels and confidence scores
-
-    classIds = []
-    confidences = []
-    boxes = []
-    for result in results:
-        for detection in result:
-            scores = detection[5:]
-            classId = np.argmax(scores)
-            confidence = scores[classId]
-            if confidence > threshold_confidence:
-                center_x = int(detection[0] * frameWidth)
-                center_y = int(detection[1] * frameHeight)
-                width = int(detection[2] * frameWidth)
-                height = int(detection[3] * frameHeight)
-                left = int(center_x - width / 2)
-                top = int(center_y - height / 2)
-                classIds.append(classId)
-                confidences.append(float(confidence))
-                boxes.append([left, top, width, height])
-
-    # Perform non maximum suppression to eliminate redundant overlapping boxes with
-    # lower confidences
-    classIds_nms = []
-    confidences_nms = []
-    boxes_nms = []
-
-    indices = cv2.dnn.NMSBoxes(boxes, confidences, threshold_confidence, threshold_nms)
-    for i in indices:
-        i = i[0]
-        classIds_nms.append(classIds[i])
-        confidences_nms.append(confidences[i])
-        boxes_nms.append(boxes[i])
-
-    # return post processed lists of classIds, confidences and bounding boxes
-    return (classIds_nms, confidences_nms, boxes_nms)
-
-################################################################################
-# Get the names of the output layers of the CNN network
-# net : an OpenCV DNN module network object
-def getOutputsNames(net):
-    # Get the names of all the layers in the network
-    layersNames = net.getLayerNames()
-    # Get the names of the output layers, i.e. the layers with unconnected outputs
-    return [layersNames[i[0] - 1] for i in net.getUnconnectedOutLayers()]
-
 
 
 
@@ -179,7 +117,6 @@ def getOutputsNames(net):
 # Initialisation of dataset
 
 # where is the data?
-
 master_path_to_dataset = os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + "\z-resources\TTBB-durham-02-10-17-sub10";
 master_path_to_yolo_resources = os.path.dirname(os.path.realpath(__file__)) + "\yolo resources"; 
 directory_to_cycle_left = "left-images";     
@@ -192,35 +129,6 @@ full_path_directory_right =  os.path.join(master_path_to_dataset, directory_to_c
 # get a list of the left image files and sort them (by timestamp in filename)
 left_file_list = sorted(os.listdir(full_path_directory_left));
 
-
-
-################################################################################
-# Initialisation of YOLO Object Detection
-
-# init YOLO CNN object detection model 
-confThreshold = 0.5  # Confidence threshold
-nmsThreshold = 0.4   # Non-maximum suppression threshold
-inpWidth = 416       # Width of network's input image
-inpHeight = 416      # Height of network's input image
-
-# Load names of classes from file
-classesFile = os.path.join(master_path_to_yolo_resources, "coco.names");
-classes = None
-with open(classesFile, 'rt') as f:
-    classes = f.read().rstrip('\n').split('\n')
-
-# load configuration and weight files for the model and load the network using them
-modelConfiguration = os.path.join(master_path_to_yolo_resources, "yolov3.cfg");
-modelWeights = os.path.join(master_path_to_yolo_resources, "yolov3.weights");
-
-net = cv2.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
-output_layer_names = getOutputsNames(net)
-
- # defaults DNN_BACKEND_INFERENCE_ENGINE if Intel Inference Engine lib available or DNN_BACKEND_OPENCV otherwise
-net.setPreferableBackend(cv2.dnn.DNN_BACKEND_DEFAULT)
-
-# change to cv2.dnn.DNN_TARGET_CPU (slower) if this causes issues (should fail gracefully if OpenCL not available)
-net.setPreferableTarget(cv2.dnn.DNN_TARGET_OPENCL)
 
 
 
@@ -245,7 +153,7 @@ image_cent4re_w = 474.5;
 ################################################################################
 # Define display window name + trackbar
 
-windowName = 'Object distance detection: ' + modelWeights
+windowName = 'Object distance detection'
 cv2.namedWindow(windowName, cv2.WINDOW_NORMAL)
 trackbarName = 'reporting confidence > (x 0.01)'
 cv2.createTrackbar(trackbarName, windowName , 0, 100, on_trackbar)
@@ -291,18 +199,10 @@ for filename_left in left_file_list:
         ################################################################################
         # YOLO Object Detection Calculations + Function Calls
         ################################################################################
-        # create a 4D tensor (OpenCV 'blob') from image frame (pixels scaled 0->1, image resized)
-        blob = cv2.dnn.blobFromImage(imgL, 1/255, (inpWidth, inpHeight), [0,0,0], 1, crop=False)
-
-        # set the input to the CNN network
-        net.setInput(blob)
-
-        # runs forward inference to get output of the final output layers
-        results = net.forward(output_layer_names)
-
-        # remove the bounding boxes with low confidence
+        
+        # Calculate threshold to remove the bounding boxes with low confidence
         confThreshold = cv2.getTrackbarPos(trackbarName,windowName) / 100
-        classIDs, confidences, boxes = postprocess(imgL, results, confThreshold, nmsThreshold)
+        classIDs, confidences, boxes = yolo.yolo(imgL, confThreshold)
 
 
         ################################################################################
---
+++ distance-quantifier-master/yolo_detection.py
@@ -0,0 +1,143 @@
+################################################################################
+
+# Example : performs YOLO (v3) object detection from a single image
+# - Detects object present within an image using YOLO
+# - Returns the boxes detailing the location of objects
+
+# Author : Toby Breckon, toby.breckon@durham.ac.uk
+# Edited by: Calum Johnston, calum.p.johnston@durham.ac.uk
+
+# License : LGPL - http://www.gnu.org/licenses/lgpl.html
+
+# Implements the You Only Look Once (YOLO) object detection architecture decribed in full in:
+# Redmon, J., & Farhadi, A. (2018). Yolov3: An incremental improvement. arXiv preprint arXiv:1804.02767.
+# https://pjreddie.com/media/files/papers/YOLOv3.pdf
+
+# To use first download the following files:
+    
+# https://pjreddie.com/media/files/yolov3.weights
+# https://github.com/pjreddie/darknet/blob/master/cfg/yolov3.cfg?raw=true
+# https://github.com/pjreddie/darknet/blob/master/data/coco.names?raw=true
+
+################################################################################
+
+import cv2
+import os
+
+################################################################################
+
+keep_processing = True
+
+#####################################################################
+# Remove the bounding boxes with low confidence using non-maxima suppression
+# image: image detection performed on
+# results: output from YOLO CNN network
+# threshold_confidence: threshold on keeping detection
+# threshold_nms: threshold used in non maximum suppression
+
+def postprocess(image, results, threshold_confidence, threshold_nms):
+    frameHeight = image.shape[0]
+    frameWidth = image.shape[1]
+
+    classIds = []
+    confidences = []
+    boxes = []
+
+    # Scan through all the bounding boxes output from the network and..
+    # 1. keep only the ones with high confidence scores.
+    # 2. assign the box class label as the class with the highest score.
+    # 3. construct a list of bounding boxes, class labels and confidence scores
+
+    classIds = []
+    confidences = []
+    boxes = []
+    for result in results:
+        for detection in result:
+            scores = detection[5:]
+            classId = np.argmax(scores)
+            confidence = scores[classId]
+            if confidence > threshold_confidence:
+                center_x = int(detection[0] * frameWidth)
+                center_y = int(detection[1] * frameHeight)
+                width = int(detection[2] * frameWidth)
+                height = int(detection[3] * frameHeight)
+                left = int(center_x - width / 2)
+                top = int(center_y - height / 2)
+                classIds.append(classId)
+                confidences.append(float(confidence))
+                boxes.append([left, top, width, height])
+
+    # Perform non maximum suppression to eliminate redundant overlapping boxes with
+    # lower confidences
+    classIds_nms = []
+    confidences_nms = []
+    boxes_nms = []
+
+    indices = cv2.dnn.NMSBoxes(boxes, confidences, threshold_confidence, threshold_nms)
+    for i in indices:
+        i = i[0]
+        classIds_nms.append(classIds[i])
+        confidences_nms.append(confidences[i])
+        boxes_nms.append(boxes[i])
+
+    # return post processed lists of classIds, confidences and bounding boxes
+    return (classIds_nms, confidences_nms, boxes_nms)
+
+################################################################################
+# Get the names of the output layers of the CNN network
+# net : an OpenCV DNN module network object
+
+def getOutputsNames(net):
+    # Get the names of all the layers in the network
+    layersNames = net.getLayerNames()
+    # Get the names of the output layers, i.e. the layers with unconnected outputs
+    return [layersNames[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+
+
+# where is the data?
+master_path_to_yolo_resources = os.path.dirname(os.path.realpath(__file__)) + "\yolo resources"; 
+
+# init YOLO CNN object detection model 
+confThreshold = 0.5  # Confidence threshold
+nmsThreshold = 0.4   # Non-maximum suppression threshold
+inpWidth = 416       # Width of network's input image
+inpHeight = 416      # Height of network's input image
+
+# Load names of classes from file
+classesFile = os.path.join(master_path_to_yolo_resources, "coco.names");
+classes = None
+with open(classesFile, 'rt') as f:
+    classes = f.read().rstrip('\n').split('\n')
+
+# load configuration and weight files for the model and load the network using them
+modelConfiguration = os.path.join(master_path_to_yolo_resources, "yolov3.cfg");
+modelWeights = os.path.join(master_path_to_yolo_resources, "yolov3.weights");
+
+net = cv2.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
+output_layer_names = getOutputsNames(net)
+
+ # defaults DNN_BACKEND_INFERENCE_ENGINE if Intel Inference Engine lib available or DNN_BACKEND_OPENCV otherwise
+net.setPreferableBackend(cv2.dnn.DNN_BACKEND_DEFAULT)
+
+# change to cv2.dnn.DNN_TARGET_CPU (slower) if this causes issues (should fail gracefully if OpenCL not available)
+net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
+
+
+
+
+def yolo(img, threshold):
+    # create a 4D tensor (OpenCV 'blob') from image frame (pixels scaled 0->1, image resized)
+    blob = cv2.dnn.blobFromImage(img, 1/255, (inpWidth, inpHeight), [0,0,0], 1, crop=False)
+
+    # set the input to the CNN network
+    net.setInput(blob)
+
+    # runs forward inference to get output of the final output layers
+    results = net.forward(output_layer_names)
+
+    # remove the bounding boxes with low confidence
+    confThreshold = threshold
+    classIDs, confidences, boxes = postprocess(img, results, confThreshold, nmsThreshold)
+
+    return classIDs, confidences, boxes
